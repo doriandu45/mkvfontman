5d32b31741aec27c2e74db7442704f366093797d
doriandu45 <d45.poubelle@gmail.com>
2021-08-25 00:35:16 +0200

    Fixed font detection when multiple font are on the same line

--- mkvfontman.sh
+++ mkvfontman.sh
@@ -6,7 +6,7 @@ function printFontsAss() {
 	# Main styles
 	grep '^Style: *' "$1" | awk -F',' '{print $2}'
 	# Fonts embedded in lines with "\fn"
-	sed -En 's/.*\\fn([^\\}]*)[\\}].*/\1/gp' "$1"
+	grep -Eho '\\fn[^\\}]*[\\}]' "$1" | sed -E 's/\\fn(.*)./\1/'
 }
 
 printFontsAss "$1"
